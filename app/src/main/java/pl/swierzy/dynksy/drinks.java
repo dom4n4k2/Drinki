f58845170a64d47e35f499b22b0c22ceffb9bbed
Sebastian Swierzewski <sebastian.swierzewski.7@gmail.com>
2021-02-27 16:45:13 +0100

    27/02 - putted expandable list from another projects, works, no changes atm

--- app/src/main/java/pl/swierzy/dynksy/drinks.java
+++ app/src/main/java/pl/swierzy/dynksy/drinks.java
@@ -5,13 +5,23 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.content.Context;
-
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class drinks extends MainActivity {
+
+
+    private ExpandableListView expandableListView;
+    private ExpandableListAdapter expandableListAdapter;
+    private List<String> expandableListNombres;
+    private HashMap<String, Contacto> listaContactos;
+    private int lastExpandedPosition = -1;
+
 
     public void drinks_back_void(View v){
         Toast.makeText(getApplicationContext(),"DRINKS ACTIVITY", Toast.LENGTH_SHORT).show();
@@ -31,6 +41,21 @@ public class drinks extends MainActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drinks_layout);
         back = (Button) findViewById(R.id.drinks_button_back);
+
+
+        init();
+
+        expandableListView.setAdapter(expandableListAdapter);
+
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                if(lastExpandedPosition != -1 && groupPosition != lastExpandedPosition){
+                    expandableListView.collapseGroup(lastExpandedPosition);
+                }
+                lastExpandedPosition = groupPosition;
+            }
+        });
 
 
         TextView tv = (TextView) findViewById(R.id.textView_drinks);
@@ -70,5 +95,59 @@ public class drinks extends MainActivity {
                     "  " +d.getInstruction_drink());
         }
 
+    }
+
+
+    private void init() {
+        this.expandableListView = findViewById(R.id.expandableListView);
+        this.listaContactos = getContactos();
+        this.expandableListNombres = new ArrayList<>(listaContactos.keySet());
+        this.expandableListAdapter = new CustomExpandableListAdapter(this,
+                expandableListNombres, listaContactos);
+
+    }
+
+
+    private HashMap<String, Contacto> getContactos() {
+        HashMap<String, Contacto> listaC = new HashMap<>();
+
+        listaC.put("Juan", new Contacto("111-111-111",
+                "juan@correo.com", "Calle 1, 11 - 4D", R.drawable.img_11));
+        listaC.put("Sara", new Contacto("222-222-222",
+                "sara@correo.com", "Calle 2, 1 - 1A", R.drawable.img_22));
+        listaC.put("Raquel", new Contacto("333-333-333",
+                "raquel@correo.com", "Calle 3, 3 - 6B", R.drawable.img_33));
+        listaC.put("Juan 2", new Contacto("444-444-444",
+                "juan@correo.com", "Calle 1, 11 - 4D", R.drawable.img_11));
+        listaC.put("Sara 2", new Contacto("555-555-555",
+                "sara@correo.com", "Calle 2, 1 - 1A", R.drawable.img_22));
+        listaC.put("Raquel 2", new Contacto("666-666-666",
+                "raquel@correo.com", "Calle 3, 3 - 6B", R.drawable.img_33));
+        listaC.put("Juan 3", new Contacto("777-777-777",
+                "juan@correo.com", "Calle 1, 11 - 4D", R.drawable.img_11));
+        listaC.put("Sara 3", new Contacto("888-888-888",
+                "sara@correo.com", "Calle 2, 1 - 1A", R.drawable.img_22));
+        listaC.put("Raquel 3", new Contacto("999-999-999",
+                "raquel@correo.com", "Calle 3, 3 - 6B", R.drawable.img_33));
+        listaC.put("Juan5", new Contacto("111-111-111",
+                "juan@correo.com", "Calle 1, 11 - 4D", R.drawable.img_11));
+        listaC.put("Sara5", new Contacto("222-222-222",
+                "sara@correo.com", "Calle 2, 1 - 1A", R.drawable.img_22));
+        listaC.put("Raquel5", new Contacto("333-333-333",
+                "raquel@correo.com", "Calle 3, 3 - 6B", R.drawable.img_33));
+        listaC.put("Juan 5", new Contacto("444-444-444",
+                "juan@correo.com", "Calle 1, 11 - 4D", R.drawable.img_11));
+        listaC.put("Sara 5", new Contacto("555-555-555",
+                "sara@correo.com", "Calle 2, 1 - 1A", R.drawable.img_22));
+        listaC.put("Raquel 5", new Contacto("666-666-666",
+                "raquel@correo.com", "Calle 3, 3 - 6B", R.drawable.img_33));
+        listaC.put("Juan 5", new Contacto("777-777-777",
+                "juan@correo.com", "Calle 1, 11 - 4D", R.drawable.img_11));
+        listaC.put("Sara 5", new Contacto("888-888-888",
+                "sara@correo.com", "Calle 2, 1 - 1A", R.drawable.img_22));
+        listaC.put("Raquel 5", new Contacto("999-999-999",
+                "raquel@correo.com", "Calle 3, 3 - 6B", R.drawable.img_33));
+
+        return listaC;
     }
 }
