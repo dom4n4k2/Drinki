07d5475e4e33126cdc62c4554cb5e3ec2e81bd02
Sebastian Swierzewski <sebastian.swierzewski.7@gmail.com>
2021-03-02 18:26:06 +0100

    02/03 added pictures, modifed excell. need to add polish letters

--- app/src/main/java/pl/swierzy/dynksy/CustomExpandableListAdapter.java
+++ app/src/main/java/pl/swierzy/dynksy/CustomExpandableListAdapter.java
@@ -1,7 +1,6 @@
 package pl.swierzy.dynksy;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +11,8 @@ import android.widget.TextView;
 
 import java.util.LinkedHashMap;
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class CustomExpandableListAdapter extends BaseExpandableListAdapter {
 
@@ -44,77 +45,59 @@ public class CustomExpandableListAdapter extends BaseExpandableListAdapter {
 
         TextView S1 = convertView.findViewById(R.id.S1);
         S1.setText(drink.getS1_drink());
-        if(drink.getS1_drink() == "") {
-            
+        if(drink.getS1_drink().isEmpty()) {
             S1.getLayoutParams().height = 0;
-            Log.d("wtf", "in if ");
         }
-        Log.d("wtf", "S1 set to " + drink.getS1_drink());
         
         TextView S2 = convertView.findViewById(R.id.S2);
         S2.setText(drink.getS2_drink());
-        if(drink.getS2_drink() == "") {
+        if(drink.getS2_drink().isEmpty()) {
             S2.getLayoutParams().height = 0;
-            Log.d("wtf", "in if ");;
         }
-        Log.d("wtf", "S2 set to " + drink.getS2_drink());
         
         TextView S3 = convertView.findViewById(R.id.S3);
         S3.setText(drink.getS3_drink());
-        if(drink.getS3_drink() == "") {
+        if(drink.getS3_drink().isEmpty()) {
             S3.getLayoutParams().height = 0;
-            Log.d("wtf", "in if ");
         }
-        Log.d("wtf", "S3 set to " + drink.getS3_drink());
+
         
         TextView S4 = convertView.findViewById(R.id.S4);
         S4.setText(drink.getS4_drink());
         if(drink.getS4_drink().isEmpty()) {
             S4.getLayoutParams().height = 0;
-            Log.d("wtf", "in if ");
         }
-        Log.d("wtf", "S4 set to " + drink.getS4_drink());
 
         TextView S5 = convertView.findViewById(R.id.S5);
         S5.setText(drink.getS5_drink());        
         if(drink.getS5_drink().isEmpty()) {
             S5.getLayoutParams().height = 0;
-            Log.d("wtf", "in if ");
         }
-        Log.d("wtf", "S5 set to " + drink.getS5_drink());
 
         TextView S6 = convertView.findViewById(R.id.S6);
         S6.setText(drink.getS6_drink());
-        if(drink.getS6_drink() == "") {
+        if(drink.getS6_drink().isEmpty()) {
             S6.getLayoutParams().height = 0;
-            Log.d("wtf", "in if ");
         }
 
-        Log.d("wtf", "S6 set to " + drink.getS6_drink());
         
         TextView S7 = convertView.findViewById(R.id.S7);
         S7.setText(drink.getS7_drink());
-        if(drink.getS7_drink() == "") {
+        if(drink.getS7_drink().isEmpty()) {
             S7.getLayoutParams().height = 0;
-            Log.d("wtf", "in if ");
         }
-        Log.d("wtf", "S7 set to " + drink.getS7_drink());
         
         TextView S8 = convertView.findViewById(R.id.S8);
         S8.setText(drink.getS8_drink());
-        if(drink.getS8_drink() == "") {
+        if(drink.getS8_drink().isEmpty()) {
             S8.getLayoutParams().height = 0;
-            Log.d("wtf", "in if ");
         }
-        Log.d("wtf", "S8 set to " + drink.getS8_drink());
         
         TextView instruction = convertView.findViewById(R.id.insruction);
         instruction.setText(drink.getInstruction_drink());
-        if(drink.getInstruction_drink() == "") {
+        if(drink.getInstruction_drink().isEmpty()) {
             instruction.getLayoutParams().height = 0;
-            Log.d("wtf", "in if ");
         }
-        Log.d("wtf", "Instr set to " + drink.getInstruction_drink());
 
 
        /* LinearLayout layoutLlamar = convertView.findViewById(R.id.lLlamar);
@@ -155,9 +138,11 @@ public class CustomExpandableListAdapter extends BaseExpandableListAdapter {
 
         }
 
+
+        CircleImageView circleImageView = convertView.findViewById(R.id.circleIMG);
+
+        circleImageView.setImageResource(context.getResources().getIdentifier(contacto.getPhoto_drink(),"drawable", context.getPackageName()));
         TextView Nazwa_drinka = convertView.findViewById(R.id.Nazwa_drinka);
-
-
         Nazwa_drinka.setText(nombre);
 
 
